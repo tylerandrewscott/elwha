4eded59a6a98395ca4d40bc2a64eb534bebff77b
tylerascott <tscott1@uw.edu>
2015-04-11 12:15:14 -0700

    fixed loglik code

--- PSJ_Submission/Version3/grab_modobjects.R
+++ PSJ_Submission/Version3/grab_modobjects.R
@@ -1,25 +1,34 @@
 rm(list=ls())
 setwd('/homes/tscott1/win/user/elwha/PSJ_Submission/Version3/')
+library(statnet)
 
 mod.names = c('mod_base','mod_allpart','mod_allpartquad',
               'mod_dirpart','mod_indpart','mod_shapart','modpast_allpart7')
 
 load('result_base.RData')
-load('result_allpart.RData')
-load('result_indpart.RData')
-load('result_dirpart.RData')
-load('result_shapart.RData')
-load('result_pasttie.RData')
-
+mod_base <- logLik.ergm(mod_base,add=TRUE)
 rm(list=ls()[ls() %in% mod.names==FALSE])
 
-mod_base <- logLik(mod_base,add=TRUE)
-mod_allpart <- logLik(mod_allpart,add=TRUE)
-mod_allpartquad <- logLik(mod_allpartquad,add=TRUE)
-mod_dirpart <- logLik(mod_dirpart,add=TRUE)
-mod_indpart <- logLik(mod_indpart,add=TRUE)
-mod_shapart <- logLik(mod_shapart,add=TRUE)
-modpast_allpart7 <- logLik(modpast_allpart7,add=TRUE)
+load('result_allpartmod.RData')
+mod_allpart <- logLik.ergm(mod_allpart,add=TRUE)
+mod_allpartquad <- logLik.ergm(mod_allpartquad,add=TRUE)
+rm(list=ls()[ls() %in% mod.names==FALSE])
+
+load('result_indpart.RData')
+mod_indpart <- logLik.ergm(mod_indpart,add=TRUE)
+rm(list=ls()[ls() %in% mod.names==FALSE])
+
+load('result_dirpart.RData')
+mod_dirpart <- logLik.ergm(mod_dirpart,add=TRUE)
+rm(list=ls()[ls() %in% mod.names==FALSE])
+
+load('result_shapart.RData')
+mod_shapart <- logLik.ergm(mod_shapart,add=TRUE)
+rm(list=ls()[ls() %in% mod.names==FALSE])
+
+load('result_pasttie.RData')
+modpast_allpart7 <- logLik.ergm(modpast_allpart7,add=TRUE)
+rm(list=ls()[ls() %in% mod.names==FALSE])
 
 save.image('only_model_results.RData')
 
