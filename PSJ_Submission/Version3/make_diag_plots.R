e4e3bc7aee83e5e0682db203c655881b6351dd31
tylerascott <tyler.scott@uga.edu>
2015-04-17 08:41:37 -0700

    update plot files

--- PSJ_Submission/Version3/make_diag_plots.R
+++ PSJ_Submission/Version3/make_diag_plots.R
@@ -7,7 +7,7 @@ rm(list=ls())
 setwd('/homes/tscott1/win/user/elwha/PSJ_Submission/Version3/')
 load('only_model_results.RData')
 
-load('result_base.RData')
+#load('result_base.RData')
 
 temp2<-ggs(mod_base$sample)
 temp2<-(merge(temp2,mod_base$target.stats))
@@ -29,8 +29,8 @@ levels(temp2$Parameter)
 ggdu = ggplot(data=temp2,aes(colour=as.character(Chain),x=Iteration,y=value))+geom_line(aes(),size=.10,alpha=.75)+
   facet_wrap(~Parameter,scales="free")+
   scale_colour_brewer(type="qual",palette='Set1',guide=guide_legend(title="MCMC Chain", label.position="bottom",nrow=2,override.aes=list(size=1.5,alpha=1)))+
-  scale_x_continuous("Iteration",expand=c(0,0))+
-  scale_y_continuous("Parameter Value",expand=c(0,0))+
+  scale_x_continuous("Iteration",expand=c(0,0),breaks = c(10000,20000),labels=c('10000','20000'))+
+  scale_y_continuous("Parameter Value")+
   theme_bw()+
   theme(axis.text=element_text(size=10),
         axis.title = element_text(size=12),
@@ -39,7 +39,7 @@ ggdu = ggplot(data=temp2,aes(colour=as.character(Chain),x=Iteration,y=value))+ge
         legend.position=c(.85,.15),legend.text.align=.5,
         axis.text=element_text(size=6),legend.title.align=.5, legend.key.size=unit(.5,'cm'),
         legend.background = element_rect(colour = "black"))
-ggsave(ggdu,filename="traceplotdu.png")
+ggsave(ggdu,filename="traceplotdu.png",dpi=400)
 rm(ggdu)
 
 
@@ -50,7 +50,7 @@ ggdudens = ggplot(data=temp2,aes(colour=as.character(Chain),x=value))+
                       guide=guide_legend(title="MCMC Chain", label.position="bottom",nrow=2,
                                          override.aes=list(size=.5,alpha=1)))+
   scale_x_continuous("Iteration",expand=c(0,0))+
-  scale_y_continuous("p(X)",expand=c(0,0)) +
+  scale_y_continuous("p(X)") +
   theme_bw()+
   theme(axis.text=element_text(size=10),
         axis.title = element_text(size=12),
@@ -60,6 +60,6 @@ ggdudens = ggplot(data=temp2,aes(colour=as.character(Chain),x=value))+
         legend.title.align=.5, legend.key.size=unit(.5,'cm'),
         axis.text=element_text(size=6),
         legend.background = element_rect(colour = "black"))
-ggsave(ggdudens,filename="densityplotdu.png")
+ggsave(ggdudens,filename="densityplotdu.png",dpi=400)
 rm(ggdudens)
 
